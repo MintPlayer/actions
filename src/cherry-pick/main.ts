b057e0367c5662364eaac3d1b79f97aa6f4d3691
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-11-21 11:25:42 +0100

    Update cherry-pick-action

--- src/cherry-pick/main.ts
+++ src/cherry-pick/main.ts
@@ -1,8 +1,23 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
+
 export async function run(ms: number) {
     console.log(`Waiting for ${ms} ms`);
 
     await new Promise((resolve) => {
         setTimeout(() => resolve(true), ms);
+    });
+
+    await exec.exec('git', ['branch'], {
+        listeners: {
+            stdout: (data) => {
+                console.log(`Found branch: ${data}`);
+            },
+            stderr: (data) => {
+                console.error('Got error', data);
+            }
+        }
     });
 
     console.log(`Done`);
