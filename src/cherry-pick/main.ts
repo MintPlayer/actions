63fb0bc0ebb0f405d6c352fc9c8cb4dc8a2517e8
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-11-21 13:16:31 +0100

    Comment out js git init

--- src/cherry-pick/main.ts
+++ src/cherry-pick/main.ts
@@ -9,26 +9,26 @@ export async function run(ms: number) {
         setTimeout(() => resolve(true), ms);
     });
 
-    await exec.exec('git', ['config', '--global', 'user.email', '"action@github.com"'], {
-        listeners: {
-            stdout: (data) => {
-                console.log(`git config ok: ${data}`);
-            },
-            stderr: (data) => {
-                console.error('git config error', data);
-            }
-        }
-    });
-    await exec.exec('git', ['config', '--global', 'user.name', '"GitHub Action"'], {
-        listeners: {
-            stdout: (data) => {
-                console.log(`git config ok: ${data}`);
-            },
-            stderr: (data) => {
-                console.error('git config error', data);
-            }
-        }
-    });
+    // await exec.exec('git', ['config', '--global', 'user.email', '"action@github.com"'], {
+    //     listeners: {
+    //         stdout: (data) => {
+    //             console.log(`git config ok: ${data}`);
+    //         },
+    //         stderr: (data) => {
+    //             console.error('git config error', data);
+    //         }
+    //     }
+    // });
+    // await exec.exec('git', ['config', '--global', 'user.name', '"GitHub Action"'], {
+    //     listeners: {
+    //         stdout: (data) => {
+    //             console.log(`git config ok: ${data}`);
+    //         },
+    //         stderr: (data) => {
+    //             console.error('git config error', data);
+    //         }
+    //     }
+    // });
 
     await exec.exec('git', ['branch'], {
         listeners: {
