7cdbc3082f6835f475461c473fe3b93a4c0eb03c
GitHub Action <action@github.com>
2023-11-21 13:07:16 +0000

    Pack with dependencies to dists

--- dist/cherry-pick/index.js
+++ dist/cherry-pick/index.js
@@ -85,27 +85,7 @@ function run(ms) {
         yield new Promise((resolve) => {
             setTimeout(() => resolve(true), ms);
         });
-        // await exec.exec('git', ['config', '--global', 'user.email', '"action@github.com"'], {
-        //     listeners: {
-        //         stdout: (data) => {
-        //             console.log(`git config ok: ${data}`);
-        //         },
-        //         stderr: (data) => {
-        //             console.error('git config error', data);
-        //         }
-        //     }
-        // });
-        // await exec.exec('git', ['config', '--global', 'user.name', '"GitHub Action"'], {
-        //     listeners: {
-        //         stdout: (data) => {
-        //             console.log(`git config ok: ${data}`);
-        //         },
-        //         stderr: (data) => {
-        //             console.error('git config error', data);
-        //         }
-        //     }
-        // });
-        yield exec.exec('git', ['branch', '-a'], {
+        yield exec.exec('git', ['branch'], {
             listeners: {
                 stdout: (data) => {
                     console.log(`Found branch: ${data}`);
