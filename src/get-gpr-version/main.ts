dd7b19dc4cc15523feb7c5fe62b52e3638c0e6a8
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-10-10 14:35:14 +0200

    user vs org

--- src/get-gpr-version/main.ts
+++ src/get-gpr-version/main.ts
@@ -1,27 +1,32 @@
 import * as core from '@actions/core';
-import { Octokit } from '@octokit/rest';
+import * as github from '@actions/github';
 
 export function isValidPackageType(packageType: string): packageType is PackageType {
     return packageTypes.includes(packageType);
 }
 
 export async function run(package_name: string, package_type: PackageType, token: string) {
-    const octokit = new Octokit({
-        auth: token
-    });
+    const octokit = github.getOctokit(token);
 
-    const x = await octokit.users.getAuthenticated();
+    // const x = await octokit.users.getAuthenticated();
 
-    // const versions = await octokit.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser({
-    //     package_name,
-    //     package_type
-    // });
-    // octokit.packages.getAllPackageVersionsForPackageOwnedByOrg({ })
-    // core.
+    if (github.context.repo.owner === github.context.actor) {
+        // This is a user repository
+        const versions = await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser({
+            package_name,
+            package_type
+        });
+        core.setOutput('packageInfo', versions);
+    } else {
+        // This is an organization repository
+        const versions = await octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+            package_name,
+            package_type,
+            org: github.context.repo.owner
+        });
+        core.setOutput('packageInfo', versions);
+    }
 
-    // octokit.packages.getAllPackageVersionsForPackageOwnedByOrg({ })
-
-    core.setOutput('packageInfo', x.data.type);
 }
 
 export const packageTypes: string[] = [
