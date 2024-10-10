a26d773077100bb8e014691851707a745b98a099
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-10-10 13:25:41 +0200

    get gpr version

--- src/get-gpr-version/index.ts
+++ src/get-gpr-version/index.ts
@@ -1,6 +1,13 @@
 import * as core from '@actions/core';
-import { run } from './main';
+import { isValidPackageType, packageTypes, run } from './main';
 
-const ms_string: string = core.getInput('milliseconds');
-const ms = parseInt(ms_string);
-run(ms);
+const package_name: string = core.getInput('package_name');
+const package_type: string = core.getInput('package_type');
+
+if (!isValidPackageType(package_type)) {
+    const message = `package_type should be one of [${packageTypes.join(', ')}]`;
+    core.error(message);
+    throw message;
+}
+
+run(package_name, package_type);
--- src/get-gpr-version/main.ts
+++ src/get-gpr-version/main.ts
@@ -1,9 +1,34 @@
-export async function run(ms: number) {
-    console.log(`Waiting for ${ms} ms`);
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/rest';
 
-    await new Promise((resolve) => {
-        setTimeout(() => resolve(true), ms);
+export function isValidPackageType(packageType: string): packageType is PackageType {
+    return packageTypes.includes(packageType);
+}
+
+export async function run(package_name: string, package_type: PackageType) {
+    const octokit = new Octokit({ auth: { }});
+
+    const versions = await octokit.packages.getAllPackageVersionsForPackageOwnedByAuthenticatedUser({
+        package_name,
+        package_type
     });
 
-    console.log(`Done`);
+    core.setOutput('packageInfo', versions);
 }
+
+export const packageTypes: string[] = [
+    'npm',
+    'maven',
+    'rubygems',
+    'docker',
+    'nuget',
+    'container'
+];
+
+export type PackageType = 
+    | 'npm'
+    | 'maven'
+    | 'rubygems'
+    | 'docker'
+    | 'nuget'
+    | 'container';
